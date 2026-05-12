526b7ec049174f38ed5a619448966a9366799a03
santoshraj12 <rajsantosh9693ty@gmail.com>
2026-05-12 16:15:55 +0530

    [200~ perimeter of  rectangle2.cpp

---
+++ perimeter of  rectangle2.cpp
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+//perimeter of  rectangle 
+int main() {
+    int l,b;
+    printf(" enter the number:-");
+    scanf("%d",&l);
+    printf(" enter the number:-");
+    scanf("%d",&b);
+    int area=2*(l+b);
+    printf(" area is %d ",area);
+      return 0;
+     
+}
