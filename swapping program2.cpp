312748d8b97866b181c7701c2b14a0e6aa871adc
santoshraj12 <rajsantosh9693ty@gmail.com>
2026-05-12 15:35:41 +0530

    swapping program1.cpp

---
+++ swapping program2.cpp
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() 
+{
+    
+    // Write C code here
+    int a=7,b=8;
+    printf(" Befor swapping \n");
+    printf("a=%d\n ",a);
+    printf("b=%d\n ",b);
+    int c=a;
+        a=b;
+        b=c;
+    printf(" After swapping \n");!
+    printf("a=%d\n ",a);
+    printf("b=%d\n",b);
+}
