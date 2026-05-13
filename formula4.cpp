f6a9df508dbeb69a5590520001f84a5bf16e6635
santoshraj12 <rajsantosh9693ty@gmail.com>
2026-05-13 16:01:37 +0530

    formula4.cpp

---
+++ formula4.cpp
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+    int l,b;
+    printf(" enter the number:-");
+    scanf("%d",&l);
+    printf(" enter the number:-");
+    scanf("%d",&b);
+    int trangle=0.5*(l+b);
+    printf(" area of triangle %d ",trangle);
+    return 0;
+     //area of tran6  
+     
+}
