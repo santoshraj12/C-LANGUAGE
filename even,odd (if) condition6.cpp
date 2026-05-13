de212060ae6e6c5ea637af0459fc379c7acdfcab
santoshraj12 <rajsantosh9693ty@gmail.com>
2026-05-13 16:17:34 +0530

    even,odd (if) condition6.cpp

---
+++ even,odd (if) condition6.cpp
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void  main() {
+    // Write C code here
+    int n;
+    printf(" enter n ");
+    scanf(" %d",&n);
+    if(n%2==0)
+    {
+        if(n>10)
+    
+    printf(" even ");
+}
+else
+{
+    printf(" condition");
+   }
+
+}
